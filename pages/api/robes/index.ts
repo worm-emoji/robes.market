1377dec73c0e3c5c7da153b5568a6fc77ce82572
Luke Miles <luke@lukemil.es>
2021-08-30 22:20:55 -0700

    filter out non eth items

--- pages/api/robes/index.ts
+++ pages/api/robes/index.ts
@@ -32,7 +32,11 @@ export const fetchRobes = async () => {
   const data = await pMap(chunked, fetchRobePage, { concurrency: 2 })
   const mapped = flatten(data)
     .filter((d) => {
-      return d.sell_orders && d.sell_orders.length > 0
+      return (
+        d.sell_orders &&
+        d.sell_orders.length > 0 &&
+        d.sell_orders[0].payment_token_contract.symbol == 'ETH'
+      )
     })
     .map((a: Asset): RobeInfo => {
       return {
