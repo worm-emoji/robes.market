7bcf9c94202c82e01f8bea364333134dd6ba5643
Pierre Bertet <hello@pierre.world>
2021-09-01 01:54:55 +0100

    Use the optional chaining operator

--- pages/api/robes/index.ts
+++ pages/api/robes/index.ts
@@ -54,25 +54,20 @@ export interface RobeInfo {
 export const fetchRobes = async () => {
   const data = await pMap(chunked, fetchRobePage, { concurrency: 2 })
   const mapped = flatten(data)
-    .filter((d) => {
-      return (
-        d.sell_orders &&
-        d.sell_orders.length > 0 &&
-        d.sell_orders[0].payment_token_contract.symbol == 'ETH'
-      )
-    })
+    .filter((d) => d?.sell_orders?.[0]?.payment_token_contract.symbol === 'ETH')
     .map((a: Asset): RobeInfo => {
       return {
         id: a.token_id,
         price: Number(
           etherUtils.formatUnits(
-            BigNumber.from(a.sell_orders[0].current_price.split('.')[0]),
+            BigNumber.from(a.sell_orders[0]?.current_price.split('.')[0]),
           ),
         ),
         url: a.permalink + '?ref=0xfb843f8c4992efdb6b42349c35f025ca55742d33',
         svg: a.image_url,
       }
     })
+
   return {
     robes: orderBy(mapped, ['price', 'id'], ['asc', 'asc']),
     lastUpdate: new Date().toISOString(),
